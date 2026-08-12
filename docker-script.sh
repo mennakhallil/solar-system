08f1346e2b85b01c9c48c6d5eb14e95002548b0c
mennakhallil <menah.khalill@gmail.com>
2026-08-12 18:10:10 +0300

    recheck docker test

--- docker-script.sh
+++ docker-script.sh
@@ -11,3 +11,4 @@ docker ps -a
 docker logs solar-system
 export DOCKER_CONTAINER_ID=$(docker ps -q -f name=solar-system)
 docker exec "$DOCKER_CONTAINER_ID" npm test 
+wget -q -O  127.0.0.1:3000/live | grep "Server is live"
