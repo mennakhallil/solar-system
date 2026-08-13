665ea149540d56ec2cce6d140a0f73e8b5734015
mennakhallil <menah.khalill@gmail.com>
2026-08-13 21:24:15 +0300

    Fix: Make private route table route conditional on nat_gateway_id being provided

--- terraform/modules/route-table/main.tf
+++ terraform/modules/route-table/main.tf
@@ -11,10 +11,15 @@ resource "aws_route_table" "public_route_table" {
 
 resource "aws_route_table" "private_route_table" {
   vpc_id = var.vpc_id
-  route {
-    cidr_block      = "0.0.0.0/0"
-    nat_gateway_id  = var.nat_gateway_id
+  
+  dynamic "route" {
+    for_each = var.nat_gateway_id != "" ? [1] : []
+    content {
+      cidr_block      = "0.0.0.0/0"
+      nat_gateway_id  = var.nat_gateway_id
+    }
   }
+  
   tags = {
     Name = var.private_route_table_name
   }
